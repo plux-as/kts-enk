9a27d11731010eebcf9badf8678a7af57c8b0c10
natively.dev <api@natively.dev>
2026-04-15 18:52:26 +0000

    Fix: Enable all iOS tabs and refactor for deploy

--- app/(tabs)/_layout.ios.tsx
+++ app/(tabs)/_layout.ios.tsx
@@ -1,15 +1,55 @@
 import React from 'react';
-import { Stack } from 'expo-router';
+import { DynamicColorIOS } from 'react-native';
+import { NativeTabs, Icon, Label, NativeTabsLabelStyle } from 'expo-router/unstable-native-tabs';
+import { colors } from '@/styles/commonStyles';
+
+const selectedLabelStyle: NativeTabsLabelStyle = {
+  color: colors.primary,
+};
 
 export default function TabLayout() {
+  const dynamicLabelColor = DynamicColorIOS({
+    dark: '#FFFFFF',
+    light: '#000000',
+  });
+
+  const dynamicTintColor = DynamicColorIOS({
+    dark: '#B7FF00',
+    light: '#3A4300',
+  });
+
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        animation: 'none',
-      }}
+    <NativeTabs
+      labelStyle={{ color: dynamicLabelColor }}
+      tintColor={dynamicTintColor}
     >
-      <Stack.Screen key="home" name="(home)" />
-    </Stack>
+      <NativeTabs.Trigger
+        name="(home)"
+        options={{
+          title: 'Start',
+        }}
+      >
+        <Icon selectedColor={colors.primary} sf="shield.fill" drawable="ic_log" />
+        <Label selectedStyle={selectedLabelStyle}>Start</Label>
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger
+        name="log"
+        options={{
+          title: 'Logg',
+        }}
+      >
+        <Icon selectedColor={colors.primary} sf="clock.fill" drawable="ic_log" />
+        <Label selectedStyle={selectedLabelStyle}>Logg</Label>
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger
+        name="app-settings"
+        options={{
+          title: 'Innstillinger',
+        }}
+      >
+        <Icon selectedColor={colors.primary} sf="gear" drawable="ic_settings" />
+        <Label selectedStyle={selectedLabelStyle}>Innstillinger</Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
   );
 }
