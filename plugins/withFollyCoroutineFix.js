d977c71747ce2384937a03fa6a94329d401ebe0f
natively.dev <api@natively.dev>
2026-04-21 08:19:50 +0000

    fix: Use Xcode 16.2 and patch Podfile for override error

--- plugins/withFollyCoroutineFix.js
+++ plugins/withFollyCoroutineFix.js
@@ -18,6 +18,21 @@ module.exports = function withFollyCoroutineFix(config) {
         config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] << 'FOLLY_CFG_NO_COROUTINES=1'
       end
     end
+  end
+
+  # Fix reanimated override warning treated as error on newer Xcode
+  # (non-virtual member function marked 'override' hides virtual member function in ReanimatedMountHook.h)
+  installer.pods_project.targets.each do |target|
+    if target.name == 'RNReanimated'
+      target.build_configurations.each do |config|
+        config.build_settings['WARNING_CFLAGS'] ||= ['$(inherited)']
+        config.build_settings['WARNING_CFLAGS'] << '-Wno-inconsistent-missing-override'
+        config.build_settings['OTHER_CPLUSPLUSFLAGS'] ||= ['$(inherited)']
+        config.build_settings['OTHER_CPLUSPLUSFLAGS'] << '-Wno-inconsistent-missing-override'
+        config.build_settings['GCC_TREAT_WARNINGS_AS_ERRORS'] = 'NO'
+        config.build_settings['CLANG_WARN_OBJCPP_ARC_EXCEPTIONS'] = 'NO'
+      end
+    end
   end`;
 
       let newContents;
